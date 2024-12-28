e12edd048621a2855ed51b152ee0ab14ebc44b14
Serticode <helpdeskforserticode@gmail.com>
2024-12-28 20:45:43 +0100

    Updates to readme.md

---
+++ lib/frontend/mobile/landing_page/mobile_landing_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:serti0x/frontend/shared/app_strings.dart';
+
+class MobileLandingPage extends ConsumerStatefulWidget {
+  const MobileLandingPage({super.key});
+  static const appStrings = AppStrings.instance;
+
+  @override
+  ConsumerState<MobileLandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends ConsumerState<MobileLandingPage>
+    with TickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.purple,
+      ),
+    );
+  }
+}
